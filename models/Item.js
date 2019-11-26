2b9015d227fd34c9c3e968aa3a9275e89e2a32e8
Lisa <lsr488@gmail.com>
2019-11-26 12:59:19 -0500

    Added Schema

--- models/Item.js
+++ models/Item.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create Schema
+const ItemSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
