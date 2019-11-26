7019accdca34a1855fc06a1659dad38ca0ed405e
Lisa <lsr488@gmail.com>
2019-11-26 13:03:25 -0500

    Creating API routes for items

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // BodyParser middleware
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI; // currently `./config/keys` is a .gitignored file, so this will break once it leaves my computer
 
 // connect to MongoDB
 mongoose
 	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('MongoDB connected...'))
 	.catch(err => console.log(err));
+
+// Use Routes
+	// any URL that points to api/items will use the items variable abov for the path name
+app.use('api/items', items);
 
 // run server
 const port = process.env.PORT || 5000;
