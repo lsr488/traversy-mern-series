ff348832d70b0fda4539ee49bd8038e62e7f3d9b
Lisa <lsr488@gmail.com>
2019-12-24 10:28:36 -0500

    Responsive navbar.

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -9,3 +9,38 @@ import {
 	NavLink,
 	Container
 } from 'reactstrap';
+
+class AppNavbar extends Component {
+	state = {
+			isOpen: false
+		}
+	toggle = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Navbar color="dark" dark expand="sm" className="mb-5">
+					<Container>
+						<NavbarBrand href="/">Shopping List</NavbarBrand>
+						<NavbarToggler onClick={this.toggle} />
+						<Collapse isOpen={this.state.isOpen} navbar>
+							<Nav className="ml-auto" navbar>
+								<NavItem>
+									<NavLink href="https://github.com/lsr488/">
+										Github
+									</NavLink>
+								</NavItem>
+							</Nav>
+						</Collapse>
+					</Container>
+				</Navbar>
+			</div>
+		);
+	}
+}
+
+export default AppNavbar;
