3f480d9683c874131a4bacf6815e092764fc86a5
Lisa <lsr488@gmail.com>
2019-12-24 10:41:29 -0500

    ShoppingList component initial commit

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -9,3 +9,35 @@ import {
 	CSSTransition, TransitionGroup
 }, from 'react-transition-group';
 import uuid from 'uuid'; // DELETE ME
+
+class ShoppingList extends Component {
+	state = {
+		items: [
+			{ id: uuid(), name: 'Eggs' },
+			{ id: uuid(), name: 'Milk' },
+			{ id: uuid(), name: 'Steak' },
+			{ id: uuid(), name: 'Water' },
+		]
+	}
+
+	render() {
+		const { items } = this.state; // "destructuring"
+		return(
+			<Container>
+				<Button 
+					color="dark" 
+					style={{marginBottom: '2rem'}} 
+					onClick={() => {
+						const name = prompt('Enter Item Name');
+						if(name) {
+							this.setState(state => ({
+								items: [...state.items, { id: uuid(), name }]
+							}));
+						}
+				}}>Add Item</Button>
+			</Container>
+		);
+	}
+}
+
+export default ShoppingList;
