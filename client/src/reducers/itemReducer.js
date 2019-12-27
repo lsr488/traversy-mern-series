66c5b209aff315a307e99ef8d931d42fd44acf06
Lisa <lsr488@gmail.com>
2019-12-27 13:24:04 -0500

    Removed static item values

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -4,11 +4,8 @@ import uuid from 'uuid';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
 
 const initialState = {
-	items: [
-		{ id: uuid(), name: 'Eggs' },
-		{ id: uuid(), name: 'Milk' },
-		{ id: uuid(), name: 'Steak' },
-	],
+	items: [],
+	loading: false
 };
 
 export default function(state = initialState, action) {
