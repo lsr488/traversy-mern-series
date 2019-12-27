df7c2112ec5b4ac36f1e671e4ccfb22f0f3c01a4
Lisa <lsr488@gmail.com>
2019-12-27 12:24:52 -0500

    Delete items via redux.

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,7 +1,15 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types.js'
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
 
 export const getItems = () => {
-	return {
-		type: GET_ITEMS
-	};
-}
+  return {
+    type: GET_ITEMS
+  };
+};
+
+export const deleteItem = (id) => {
+	console.log("Action deleteItem id:", id);
+  return {
+    type: DELETE_ITEM,
+    payload: id
+  };
+};
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,7 +1,7 @@
 // where the actual state is going to go
 // where we check our actions
 import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types.js'
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
 
 const initialState = {
 	items: [
@@ -9,16 +9,22 @@ const initialState = {
 		{ id: uuid(), name: 'Milk' },
 		{ id: uuid(), name: 'Steak' },
 		{ id: uuid(), name: 'Water' },
-	]	
-}
+	],
+};
 
 export default function(state = initialState, action) {
-	switch(action.type) {
-		case GET_ITEMS: 
-			return {
-				...state
-			}
-			default:
-				return state;
-	}
+  switch (action.type) {
+    case GET_ITEMS:
+      return {
+        ...state
+      };
+    case DELETE_ITEM:
+    	console.log("Reducer DELETE_ITEM:", action.payload);
+    	return {
+    		...state,
+				items: state.items.filter(item => item.id !== action.payload)
+    	};
+    default:
+        return state
+  }
 }
